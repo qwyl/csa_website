1f44ad193c2051ba0ead6091ebe0117b7e3c8ff3
Qi Li Yang <qiyang2@berkeley.edu>
2025-07-15 23:01:33 -0500

    finish home page

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -1,7 +1,42 @@
+
+// HAS NO LINKS YET
 function Footer() {
     return (
-        <footer className="bg-red text-[#f5f5f5] p-6 font-serif flex justify-between">
-            
+        <footer className="bg-[#2F2D29] text-[#f5f5f5] py-10 px-40 flex justify-between">
+            <div className="grid grid-cols-5 gap-24">
+                <div className="col-span-2">
+                    <span className="text-white text-xl font-semibold text-nowrap">Chinese Student Association</span>
+                    <p className="text-sm">
+                    University of California, Berkeley <br/>
+                    2465 Bancroft Way <br/>
+                    Berkeley, CA 94704
+                    </p>
+                </div>
+                <div className="">
+                    <span className="text-white text-xl font-semibold">Explore</span>
+                    <p className="text-sm">
+                    About <br/>
+                    Events <br/>
+                    Our Team <br/>
+                    Join Us
+                    </p>
+                </div>
+                <div className="">
+                    <span className="text-white text-xl font-semibold">Contact</span>
+                    <p className="text-sm">
+                    Email Us <br/>
+                    Join Our Mailing List <br/>
+                    </p>
+                </div>
+                <div className="">
+                    <span className="text-white text-xl font-semibold">Connect</span>
+                    <p className="text-sm">
+                    Email Us <br/>
+                    Join Our Mailing List <br/>
+                    </p>
+                </div>
+
+            </div>
         </footer>
     );
 }
---
+++ src/pages/JoinUs.page.tsx
@@ -0,0 +1,10 @@
+function JoinUs() {
+    return (
+        <div>
+          <h1>Join Us</h1>
+          <h3>Chinese Student Association at UC Berkeley</h3>
+        </div>
+    );
+}
+
+export default JoinUs
